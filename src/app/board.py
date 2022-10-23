b5f2c37091ec0a97264e53afcbc08ab42e5fd18b
tameemalaa <tameem.alaa@gmail.com>
2022-10-23 23:38:17 +0200

    added methods to load and save board
    this commit include validation static method to use when loading a file

--- src/app/board.py
+++ src/app/board.py
@@ -21,3 +21,26 @@ class Board:
     def fill_random(self)-> None: 
         self.state = [ [choice([ALIVE , DEAD]) for _ in range(self.width)] for _ in range(self.height)]
         
+    @staticmethod
+    def validate_lines(lines: list)-> bool:
+        width = len(lines[0])
+        for i in range(lines):
+            if width != lines[i]: raise ValueError('File dimensions are not valid')
+            for j in range(lines[i]):
+                if lines[i][j] != ALIVE or lines[i][j] != DEAD: raise ValueError(f'Value in row:{i} column:{j} is not valid')
+        return True
+        
+    def fill_from_file(self)-> None:
+        if self.file:
+            lines= [ i.strip for i in self.file.readlines()]
+            if self.validate_lines(lines):
+                self.height= len(lines)
+                self.width= len(lines[0])
+                self.state= [[int(lines[i][j]) for i in range(self.height)] for j in range(self.width)] 
+        else : raise ValueError('No file has been provided')
+
+    def save_to_file(self,file: TextIO)-> None:
+        for i in range(len(self.state)):
+            for j in range(len(self.state[i])):
+                file.write(self.state[i][j])
+                if j == len(self.state[i]) -1 :file.write('\n')
